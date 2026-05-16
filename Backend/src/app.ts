467bd51754939964ac83413b2694210b65ad303b
Kamal-Krushna-Sahu <sahukamalkrushna369@gmail.com>
2026-05-16 19:21:45 +0530

    feat: enhance graph AI service with new state management and judging functionality

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import useGraph from "./services/graph.ai.service.js";
 
 const app = express();
 
@@ -6,6 +7,10 @@ app.get("/health", (req, res) => {
   res.status(200).json({
     message: "OK",
   });
+});
+
+app.post("/use-graph", async (req, res) => {
+  await useGraph("What is the capital of India?");
 });
 
 export default app;
