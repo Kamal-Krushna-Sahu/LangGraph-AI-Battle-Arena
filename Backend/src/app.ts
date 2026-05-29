28f176bc40e0076f5ce2093b76c4fec35be15536
Kamal-Krushna-Sahu <sahukamalkrushna369@gmail.com>
2026-05-29 18:33:36 +0530

    feat: add CORS support and update dependencies in package.json

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,7 +1,17 @@
 import express from "express";
 import runGraph from "./services/ai/graph.ai.service.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  }),
+);
 
 app.get("/health", (req, res) => {
   res.status(200).json({
@@ -10,9 +20,9 @@ app.get("/health", (req, res) => {
 });
 
 app.post("/run-graph", async (req, res) => {
-  const result = await runGraph(
-    "Write code for factorial function in JavaScript.",
-  );
+  const { query } = req.body;
+
+  const result = await runGraph(query);
   res.json(result);
 });
 
