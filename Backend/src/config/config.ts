9eac11d91823d516aa6f749ae0944ef1f40c7fd5
Kamal-Krushna-Sahu <sahukamalkrushna369@gmail.com>
2026-05-30 00:26:27 +0530

    feat: add build and start scripts, configure server port from environment variables
    - Added "build" and "start" scripts to package.json for building and running the server.
    - Updated server.ts to listen on a port defined in the config file.
    - Created config.ts to manage environment variables, including the server port.

--- Backend/src/config/config.ts
+++ Backend/src/config/config.ts
@@ -1,12 +1,14 @@
 import "dotenv/config";
 
 type CONFIG = {
+  readonly PORT: number;
   readonly GEMINI_API_KEY: string;
   readonly MISTRAL_API_KEY: string;
   readonly COHERE_API_KEY: string;
 };
 
 const config: CONFIG = {
+  PORT: parseInt(process.env.PORT || "4000", 10),
   GEMINI_API_KEY: process.env.GEMINI_API_KEY || "",
   MISTRAL_API_KEY: process.env.MISTRAL_API_KEY || "",
   COHERE_API_KEY: process.env.COHERE_API_KEY || "",
